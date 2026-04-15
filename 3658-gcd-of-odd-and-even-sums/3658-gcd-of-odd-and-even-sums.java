c02aac3108beaec81a9a844a8d7bb30f00e055f6
Kishore Abinash A <152689320+kishore2818@users.noreply.github.com>
2026-04-15 18:14:26 +0530

    Time: 0 ms (100%), Space: 42.7 MB (26.87%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,21 @@
+import java.math.BigInteger;
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // int odd=0;
+        // int even=0;
+        // int i=n;
+        // while(n!=0){
+        //     if(i%2==0){
+        //         even+=i;
+        //     }if(i%2==1){
+        //         odd+=i;
+        //     }
+        //     i--;
+        //     n--;
+        // }
+        // BigInteger b1=BigInteger.valueOf(odd);
+        // BigInteger b2=BigInteger.valueOf(even);
+        // int ans=b2.gcd(b1).intValue();
+        return n;
+    }
+}
