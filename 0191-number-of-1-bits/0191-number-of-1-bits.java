7730309968703381050246951256e566d6ccb1da
Kishore Abinash A <152689320+kishore2818@users.noreply.github.com>
2026-04-16 17:46:22 +0530

    Time: 1 ms (32.39%), Space: 42.5 MB (34.07%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        String a=Integer.toBinaryString(n);
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)=='1') count++;
+        }
+        return count;
+    }
+}
