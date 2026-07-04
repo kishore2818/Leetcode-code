3edb7b0a4f93dc962cbaf290aacb651b57518a76
Kishore Abinash A <152689320+kishore2818@users.noreply.github.com>
2026-07-04 10:13:40 +0530

    Time: 3 ms (20.27%), Space: 47.8 MB (25.24%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,38 @@
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int ans[][]=new int[matirx.length][matix[0].length];
+//         for(int i=0;i<matrix.length;i++){
+//             for(int j=0;j<matrix[0].length;j++){
+//                 if(matix[i][j]==0){
+//                     ans[i][j]=
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> row = new HashSet<>();
+        Set<Integer> col = new HashSet<>();
+        for(int i =0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for(int i:row){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[i][j]=0;
+            }
+        }
+        for(int i:col){
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][i]=0;
+            }
+        }
+    }
+}
