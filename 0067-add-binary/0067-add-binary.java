04febc4207fc6536dd74e936584e8f6193a53123
Kishore Abinash A <152689320+kishore2818@users.noreply.github.com>
2026-07-10 23:05:53 +0530

    Time: 6 ms (6.84%), Space: 43.8 MB (24.59%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger b1=new BigInteger(a,2);
+        BigInteger b2=new BigInteger(b,2);
+        BigInteger sum=b1.add(b2);
+
+        return sum.toString(2);
+    }
+}
