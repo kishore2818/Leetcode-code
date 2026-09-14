3df15a328fc335b2b6066f8b3052b365b2d6555b
Kishore Abinash A <152689320+kishore2818@users.noreply.github.com>
2026-09-14 22:07:16 +0530

    Time: 16 ms (46.5%), Space: 43.7 MB (74.02%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> m1=new HashMap<>();
+        HashMap<Character,Character> m2=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+
+            char a=s.charAt(i);
+            char b=t.charAt(i);
+
+            if(m1.containsKey(a)){
+                if(m1.get(a)!=b){
+                    return false;
+                }
+            }
+                else{
+                    m1.put(a,b);
+                }
+
+            if(m2.containsKey(b)){
+                if(m2.get(b)!=a){
+                    return false;
+                }
+                }else{
+                    m2.put(b,a);
+                }
+            }
+        return true;
+    }
+}
